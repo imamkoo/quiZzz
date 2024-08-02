da356b2afc680fb03204da3053639bf65bd64380
imamkoo <imamkholidubaidillah16@gmail.com>
2024-08-02 14:11:54 +0700

    feat(login): implementasi fitur login dengan autentikasi

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDV9JZyt9ZDP5vhGxcr8RI3T7DWBc7OkUU",
-  authDomain: "quiz-app-5737c.firebaseapp.com",
-  projectId: "quiz-app-5737c",
-  storageBucket: "quiz-app-5737c.appspot.com",
-  messagingSenderId: "206884772269",
-  appId: "1:206884772269:web:3810491a6699487f7ae924",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
